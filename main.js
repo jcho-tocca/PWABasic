f0c922e3933b554941f70e9f9a1134d51c67f277
jcho-tocca <jaeman.cho@systems.tocca-net.jp>
2020-08-27 18:45:47 +0900

    modify files path

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // service workerの登録
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('./service-worker.js')
         .then((reg) => {
           console.log('Service worker registered.', reg);
         }).catch(err => {
